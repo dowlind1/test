c7017867ab04ce4709081f1bcaa8262ca48b0644
Daniel Dowling <32266502+dowlind1@users.noreply.github.com>
2017-10-08 14:22:49 +0100

    Initial Test check in-https://www.youtube.com/watch?v=r5C6yXNaSGo

---
+++ test/src/test/lcg1.java
@@ -0,0 +1,15 @@
+package test;
+
+public class lcg1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x =((21*76)+3)%200;
+		int y =((21*x)+3)%200;
+		int z =((21*y)+3)%200;
+		System.out.println("x="+x);
+		System.out.println("y="+y);
+		System.out.println("z="+z);
+	}
+
+}
